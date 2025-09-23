e074f16786e2e8caa513015e039d1605205d8e36
Pongaskornjj <pongsakorn.sra@ku.th>
2025-09-23 12:05:46 +0700

    add test_random file for week09

--- assignments/week09/test_random.py
+++ assignments/week09/test_random.py
@@ -1,7 +1,22 @@
 import random
 
 def test_random():
+    # สุ่มเลข 1-10 เก็บไว้ใน random_number
     random_number = random.randint(1, 100)
-    print(random_number)
     
+    # รับค่าจากผู้ใช้ เก็บใน guess_number
+    guess_number = input("What is your guess number?: ")
+    
+    
+    if random_number == guess_number:
+        print("Congratulations")
+        
+    elif random_number < guess_number:
+        print("too much")
+        
+    elif random_number > guess_number:
+        print("too low")
+        
+    print(random_number)
+ 
 test_random()
